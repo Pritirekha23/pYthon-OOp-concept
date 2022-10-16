9a76dbb32267a065485fe3add67e9a20a063fc58
Pritirekha23 <learnpriti23@gmail.com>
2022-10-15 19:14:38 -0700

    my first commit

---
+++ 01_introduction to OOP in python.py
@@ -0,0 +1,57 @@
+# INTRODUCTION TO OOP CONCEPT IN PYTHON
+  # Till now in our programs we have been using the procedure-oriented technique which means we have designed our program by using functions of blocks of statements and these functions are used for operations or manipulations of data.
+  # This type of approach is called as procedure-oriented programming.
+  # But there is another beautiful programming paradigm called object-oriented programming.
+
+# PROGRAMMING PARADIGM:--
+    # Monolithic oriented programming (in one file u have to write all the code)---> Assembly language,BASIC
+    # Procedure/Structure/function  oriented programming(POP) ---> FORTAN(scientific operation),COBAL,PASCAL--(special purpose language),C(general purpose language)
+    # Object-oriented programming (OOP)---> C++,Python,Java,phP etc
+
+#INTRODUCTION TO OOP:-
+    # Mechanism, Paradigm or style,Approach or technique,Principle,Guideline,Methodology 
+    # OOP is principle to develope a program.
+# WHAT IS OOP?
+   # OOP is a programming paradigm to write program efficiently.
+   # In OOP we will develop a program mainly using class and object.
+   # OOP includes code reusability,scalability, and efficiency.
+   # OOP is very suitable for developing large and complex applications.
+   # OOP follows the DRY("Dont repeat yourself") approach.
+   # In OOP, we will wrap data and function into a single unit called class.
+   # Python collects object-oriented mechanishm from c++.
+# ADVANTAGES OF OOP
+    # Scalable
+    # Reusability
+    # Modularity
+    # Flexibility
+    # Security
+
+# 4 principles of OOP
+  # Encapsulation
+  # Abstraction
+  # Inheritance
+  # Polymorphism
+
+
+
+# Questions:
+   # Python support both procedure  oriented and object oriented approach?
+      # ans:-
+   # First object oriented programming language name?
+      # ans:-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
