0c859c5b4727caa208c0934340fca277c9f0b1aa
Pritirekha23 <learnpriti23@gmail.com>
2022-10-16 20:06:43 -0700

    ading 2 files

---
+++ 03_self variable.py
@@ -0,0 +1,43 @@
+# self variable 
+# what is Self?
+  # refernce variable---  access object members outside of the class
+  # self variable --- access object members inside of the class
+
+
+# By using self variable we can access attributes and methods outside of the class, but if we want to access object attributes and methods inside the class then we can use self variable.
+# self variable is used to access instance variable and instance method of an object inside the class.
+#Self is used to refer to a current class  object or current memory.
+# It is also acting as an accessor.
+# Self is the first argument for constructor and instance method.
+# self is not a keyword in python,instead of self we can write other names,but itis highly recomanded to use self.
+
+# create a class Student having 3 attributes and 1 method.
+class Student:
+    def __init__(self):
+        self.name=input('Enter your name::')
+        self.age=int(input('Enter your age::'))
+        self.course=input('Enter your course::')
+    
+    def printdetails(self):
+        print('-'*40)
+        print(f'Name is {self.name}')
+        print(f'Age is {self.age}')
+        print(f'Course is {self.course}')
+
+
+s1=Student()
+s2=Student()
+s3=Student()
+s1.printdetails()
+s2.printdetails()
+s3.printdetails()
+# at the time of object creation constructor will execute automatically.
+
+
+
+
+
+
+
+
+
---
+++ 04_extra about self.py
@@ -0,0 +1,84 @@
+class Student():
+    def __init__(self):
+        print(id(self))
+s1=Student()
+print(id(s1))
+# here self and s1 point into same memory location
+
+print('-'*10)
+s2=Student()
+print(id(s2))
+
+# 1st time s1 and self is same
+# 2nd time s2 and self is same
+# self is not a keyword in python
+# instead of self we can use anything like x,y,any name or anything.
+# But it is highly recomanded that use self .
+
+print('prove that self is a variable')
+class Student:
+    def __init__(v):
+        v.name=input('Enter your name::')
+        v.age=int(input('Enter your age::'))
+        v.course=input('Enter your course::')
+    
+    def printdetails(v):
+        print('-'*40)
+        print(f'Name is {v.name}')
+        print(f'Age is {v.age}')
+        print(f'Course is {v.course}')
+
+
+# 
+s1=Student()
+s1.printdetails()
+
+print('__________')
+class Student:
+  def __init__(v,name,age,sid):
+    v.name=name
+    v.age=age
+    v.sid=sid
+  def printdetails(v):
+    print(f'name is {v.name}')
+    print(f'age is {v.age}')
+    print(f'ssid is {v.sid}')
+s1=Student("priti",20,1056)
+s1.printdetails()
+
+
+
+
+print('----------')
+class Student:
+  def __init__(name,age,sid):
+    name.age=age
+    name.sid=sid
+  def printdetails(v):
+    print(f'name is {v.age}')
+    print(f'age is {v.sid}')
+s1=Student(23,1002546)
+s1.printdetails()
+
+
+
+
+print('----------')
+print('Here python take name as self so we get an error')
+class Student:
+  def __init__(name,age,sid):
+    v.name=name
+    v.age=age
+    v.sid=sid
+  def printdetails(v):
+    print(f'name is {v.name}')
+    print(f'age is {v.age}')
+    print(f'ssid is {v.sid}')
+s1=Student("priti",20,1056)
+#s1=Student("priti",20,1056)
+#TypeError: __init__() takes 3 positional arguments but 4 were given 
+s1.printdetails()
+
+
+
+
