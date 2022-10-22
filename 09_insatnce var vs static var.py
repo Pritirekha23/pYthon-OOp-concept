5134904325ce1e9ff401a93f4bbd16c9229bd3bf
Pritirekha23 <learnpriti23@gmail.com>
2022-10-21 20:09:28 -0700

    static variable vs instance variable

---
+++ 09_insatnce var vs static var.py
@@ -0,0 +1,15 @@
+# Instance variable vs static variable
+
+# INSTANCE VARISBLE
+    # It blenogs to Object,it also called as Object level variable.
+    # for every object separate copies of instance variable will be created.
+    # Instance variable is not shared among objects.
+    # If we made any chnages in a particular object it will not reflect to other objects.
+    # less memory utilization.
+
+# STATIC VARIABLE
+    # It belongs to class, it is also called ass class level variable.
+    # Only single copies will create per class and it will be share to all object.
+    # Static variable is shared among objects.
+    # If we made any chamges it will directly affects every object of that particular class.
+    # Better memory utilization.
