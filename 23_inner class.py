a6d3dbf5dac9d8eb275c272cc297f964548bc739
Pritirekha23 <learnpriti23@gmail.com>
2023-01-11 13:52:01 -0700

    adding inner classs

---
+++ 23_inner class.py
@@ -0,0 +1,10 @@
+#dt-11/01/23
+# Inner or nested class
+
+# if a class is present inside another class then this is called as inner class.
+# inner class always depends upon outer class.Inner class does not exist without outer class existence.
+# If there is no outer class then there is no chamce to exist inner class.
+#ex- Bank is outer class where account is inner class
+
+
+
