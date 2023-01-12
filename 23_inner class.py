4b680dfd1af165ae1d76b076b3376f8cbbf8b524
Pritirekha23 <learnpriti23@gmail.com>
2023-01-12 12:03:48 -0700

    adding has-a relationship

--- 23_inner class.py
+++ 23_inner class.py
@@ -8,7 +8,7 @@
 
 # One outer class can contain multiple inner class.
 
-
+#ex-1
 class Outer:
     def __init__(self):
         print('Outer class constructor')
@@ -33,3 +33,94 @@ i1=Outer.Inner()
 # outer class as well aas inner class object creation.
 i2=Outer().Inner()
 # Outer().Inner() --- It means 1st it will create object of Outer class then it will create object of inner class
+
+
+#output
+#Outer class constructor
+#Inner class constructor
+#Inner class constructor
+#Outer class constructor
+#Inner class constructor
+
+
+# NOTE :
+    # you can not create inner class object directly u can create it by using Outer class reference variable or outer class name.
+    # dual object creation also possible.
+
+
+#ex-2
+# inner class object creation inside outer class
+
+print('---ex-2---------')
+
+class Outer:
+    def __init__(self):
+        print('Outer class constructor')
+    class Inner:
+        def __init__(self):
+            print('Inner class constructor')
+    i5=Inner()
+O=Outer()
+
+#output:
+#---ex-2---------
+#Inner class constructor
+#Outer class constructor
+
+
+#ex-3  ---> one outer class can contain multiple inner class.
+print('---ex-3----')
+class Outer:
+    def __init__(self):
+        print('Outer class constructor')
+    class Inner:
+        def __init__(self):
+            print('Inner class constructor')
+    class Inner2:
+        def __init__(self):
+            print('Inner class2 constructor')
+
+O=Outer()
+i8=O.Inner()
+i7=O.Inner()
+
+# output
+#---ex-3----
+#Outer class constructor
+#Inner class constructor
+#Inner class constructor
+
+
+# ex-4  Inner-Inner class
+print('-----------')
+
+
+class Outer:
+    def __init__(self):
+        print('Outer class constructor')
+
+    class Inner:
+        def __init__(self):
+            print('Inner class constructor')
+
+        class InnerInner:
+            def __init__(self):
+               print('InnerInner class constructor')
+ 
+
+O = Outer()
+i9 = O.Inner()
+i34=i9.InnerInner()
+obj=Outer.Inner.InnerInner()
+#output
+#-----------
+#Outer class constructor
+#Inner class constructor
+#InnerInner class constructor
+#InnerInner class constructor
+
+
+
+
+
+
